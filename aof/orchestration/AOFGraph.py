701e1a4f1ac7aaaaaf7ac1ebcbb8b0ad34b15943
jo-tud <johannes.pfeffer@tu-dresden.de>
2015-04-02 18:55:55 +0200

    Make sure the aof namespace is always known to AOFGraph

--- aof/orchestration/AOFGraph.py
+++ aof/orchestration/AOFGraph.py
@@ -1,6 +1,7 @@
 from rdflib import Dataset, Namespace
 from rdflib.plugins.memory import IOMemory
 from aof.orchestration.Singleton import Singleton
+from aof.orchestration.namespaces import AOF
 
 __all__ = [
     'AOFGraph'
@@ -15,6 +16,9 @@ class AOFGraph(Dataset):
     def __init__(self):
         store = IOMemory() # TODO: Change the storage mechanism to a persistent one and implement caching
         Dataset.__init__(self, store=store,default_union=True)
+
+        # Make sure the aof namespace is always known to AOFGraph
+        self.bind('aof', AOF)
 
 # Will only be called when executed from shell
 if __name__ == "__main__":
