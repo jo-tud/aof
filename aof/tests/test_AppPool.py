2e0abaa31d0c2dd36c50fb6b1f1944df288a8573
khörfurter <korbinian.hoerfurter@tu-dresden.de>
2015-04-14 16:47:54 +0200

    Revised functional unittests for the AppPool for using the new app descriptions

--- aof/tests/test_AppPool.py
+++ aof/tests/test_AppPool.py
@@ -5,6 +5,8 @@ from pyramid.path import AssetResolver
 from aof.orchestration.AppPool import AppPool
 from rdflib import URIRef
 
+from aof.orchestration.namespaces import AOF, ANDROID
+
 class AppPoolTests(unittest.TestCase):
     """
 
@@ -18,8 +20,8 @@ class AppPoolTests(unittest.TestCase):
         a = AssetResolver()
         self.path = a.resolve('aof:tests/res/test_pool.ttl').abspath()
         self.ap=AppPool.Instance(source=self.path, format="turtle")
-        self.maxApp=URIRef("http://dev.plt.et.tu-dresden.de:8085/jenkins/job/AOFConductor/LastSuccessfulBuild")
-        self.minApp=URIRef("http://dev.plt.et.tu-dresden.de:8085/jenkins/job/AppEnsembleInstaller/")
+        self.maxApp=URIRef("http://mustermann.de/maxApp")
+        self.minApp=URIRef("http://mustermann.de/minApp")
 
 
     def tearDown(self):
@@ -45,16 +47,16 @@ class AppPoolTests(unittest.TestCase):
         items.sort()
         for index, uri in enumerate(items, start=1):   # default is zero
             if index==1:
-                self.assertEqual(str(uri),"http://dev.plt.et.tu-dresden.de:8085/jenkins/job/AOFConductor/LastSuccessfulBuild","The first AppUri in the AppPool isn't initialized correctly!")
+                self.assertEqual(str(uri),"http://mustermann.de/maxApp","The first AppUri in the AppPool isn't initialized correctly!")
             elif index==2:
-                self.assertEqual(str(uri),"http://dev.plt.et.tu-dresden.de:8085/jenkins/job/AppEnsembleInstaller/","The first AppUri in the AppPool isn't initialized correctly!")
+                self.assertEqual(str(uri),"http://mustermann.de/minApp","The first AppUri in the AppPool isn't initialized correctly!")
 
         self.assertIs(self.ap.get_number_of_apps(), 2, "Number of Apps in the AppPool is not correct!")
 
     def test_get_name_maxminApp(self):
         # Tests if the App_labels are correct
-        self.assertEqual(self.ap.get_name(self.maxApp),"AOF Conductor", 'Name of the "AOF Coductor" App is wrong!')
-        self.assertEqual(self.ap.get_name(self.minApp),"ComVantage IAF Login App",'Name of the "ComVantage IAF Login App" App is wrong!')
+        self.assertEqual(self.ap.get_name(self.maxApp),"MaxApp", 'Name of the "MaxApp" is wrong!')
+        self.assertEqual(self.ap.get_name(self.minApp),"MinApp",'Name of the "MinApp" is wrong!')
 
     def test_get_description_maxminApp(self):
         # test if the description of the maxApp is correct and if the minApp has none.
@@ -63,12 +65,12 @@ class AppPoolTests(unittest.TestCase):
 
     def test_get_icon_uri_maxminApp(self):
         # test if the Icon Uri of the maxApp is correct and if the minApp has none.
-        self.assertEqual(self.ap.get_icon_uri(self.maxApp),"www.testicon.de","An App with an Icon returns None!")
-        self.assertEqual(self.ap.get_icon_uri(self.minApp),"None","An App with NO Icon returns one!")
+        self.assertEqual(str(self.ap.get_icon_uri(self.maxApp)),"http://mustermann.de/maxApp/res/icon.jpg","An App with an Icon returns None!")
+        self.assertEqual(str(self.ap.get_icon_uri(self.minApp)),"None","An App with NO Icon returns one!")
 
     def test_get_binary_uri_maxApp(self):
         # test if the Binary uri of the maxApp is correct .
-        self.assertEqual(self.ap.get_binary_uri(self.maxApp),"http://dev.plt.et.tu-dresden.de:8085/jenkins/job/AOFConductor/lastSuccessfulBuild/artifact/AppEnsembleInstaller/bin/AppEnsembleInstaller-release.apk","An App with an Binary Uri returns None!")
+        self.assertEqual(self.ap.get_binary_uri(self.maxApp),"http://mustermann.de/maxApp/res/install.apk","An App with an Binary Uri returns None!")
 
     def test_hasget_role_maxminApp(self):
         # Tests if the Roles are loaded correctly
@@ -82,8 +84,8 @@ class AppPoolTests(unittest.TestCase):
         self.assertListEqual(minRoles,[],"App without roles seems to have some!")
 
     def test_is_android_app_maxminApp(self):
-        self.assertTrue(self.ap.is_android_app(self.maxApp),"Conductor should be an Android App!")
-        self.assertFalse(self.ap.is_android_app(self.minApp),"ComVantage IAF Login App (for the test case) should not be an Android App!")
+        self.assertTrue(self.ap.is_android_app(self.maxApp),"MapApp should be an Android App!")
+        self.assertFalse(self.ap.is_android_app(self.minApp),"MinApp should not be an Android App!")
 
     def test_hasget_main_screenshot_maxminApp(self):
         # Tests if the Main Screenshots are loaded correctly
@@ -93,7 +95,7 @@ class AppPoolTests(unittest.TestCase):
         maxScreenshots=self.ap.get_main_screenshot(self.maxApp)
         minScreenshots=self.ap.get_main_screenshot(self.minApp)
 
-        self.assertDictEqual(maxScreenshots,{'comment': 'None', 'uri': 'http://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Full_score.jpg/320px-Full_score.jpg'},"App roles were not correct loaded!")
+        self.assertDictEqual(maxScreenshots,{'comment': 'None', 'uri': 'http://mustermann.de/maxApp/res/mainScreenshot.jpg'},"App roles were not correct loaded!")
         self.assertDictEqual(minScreenshots,{'comment': 'None', 'uri': 'None'},"App without roles seems to have some!")
 
     def test_hasget_other_screenshot_maxminApp(self):
@@ -112,9 +114,9 @@ class AppPoolTests(unittest.TestCase):
                 break
             i+=1
         else:
-            self.assertTrue(False,"The other Screenshots of the Conductor aren't loaded correctly")
+            self.assertTrue(False,"The other Screenshots of the MaxApp aren't loaded correctly")
 
-        self.assertIs(minScreenshots.__len__(),0,"Number of other screenshots (ComVantage IAF Login App (for the test case)) is not correct")
+        self.assertIs(minScreenshots.__len__(),0,"Number of other screenshots (MinApp) is not correct")
 
     def test_hasget_creators_maxminApp(self):
         # Tests if the creators are loaded correctly
@@ -124,9 +126,9 @@ class AppPoolTests(unittest.TestCase):
         maxCreators=self.ap.get_creators(self.maxApp)
         minCreators=self.ap.get_creators(self.minApp)
 
-        self.assertIs(maxCreators.__len__(),1,"Number of Creators (Conductor app) is not correct")
-        self.assertEqual(maxCreators[0]['name'],"Johannes Pfeffer")
-        self.assertIs(minCreators.__len__(),0,"Number of Creators (ComVantage IAF Login App (for the test case)) is not correct")
+        self.assertIs(maxCreators.__len__(),1,"Number of Creators (MaxApp) is not correct")
+        self.assertEqual(maxCreators[0]['name'],"Mustermann")
+        self.assertIs(minCreators.__len__(),0,"Number of Creators (MinApp) is not correct")
 
     def test_hasget_entry_points_with_input_check_maxminApp(self):
         # Tests if the entry points are loaded correctly. Afterwards checks the Inputs for the Conductor entry point
