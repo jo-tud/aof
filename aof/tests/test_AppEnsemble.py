ae7a17994e2b13b4eab1f0e4eebeca51d607af34
khörfurter <korbinian.hoerfurter@tu-dresden.de>
2015-04-20 14:09:09 +0200

    Completed AppEnsemble Unit tests

--- aof/tests/test_AppEnsemble.py
+++ aof/tests/test_AppEnsemble.py
@@ -1,25 +1,14 @@
 import unittest
-import shutil
-import os, sys
+import os
 import zipfile
 
-
 from pyramid import testing
-
 from pyramid.path import AssetResolver
 from aof.orchestration.AppEnsemble import AppEnsemble
-from rdflib import URIRef
 
-from aof.orchestration.namespaces import AOF, ANDROID
+
 
 class AppEnsembleTests(unittest.TestCase):
-    """
-
-    TODO's:
-    - make the test_ae.ttl work!
-    - fix the AppEnsemble get_file error and make good error handling; apply testing
-    - remove_app_ensemble doesn't work and is it good to remove the .ae file??
-    """
 
     def setUp(self):
         """
@@ -34,13 +23,14 @@ class AppEnsembleTests(unittest.TestCase):
         originsPath=self.a.resolve('aof:tests/res/').abspath()
 
         # Destination of the zip archive
-        self.destTestArchive= self.a.resolve('aof:static/App-Ensembles/').abspath() + self.ae_name + '.ae'
+        self.destTestArchive= self.a.resolve(AppEnsemble.ae_folder_path).abspath() + self.ae_name + AppEnsemble.ae_extention
 
         # Creation of the zip archive
         zip_ae = zipfile.ZipFile(self.destTestArchive, mode='w')
         try:
-            zip_ae.write(originsPath + 'test_ae.ttl','aes.ttl')
-            zip_ae.write(originsPath + 'test_ae.bpmn','ae.bpmn')
+            zip_ae.write(originsPath + 'test_ae.ttl',AppEnsemble.ae_filename)
+            zip_ae.write(originsPath + 'test_ae_empty.ttl','test_ae_empty.ttl')
+            zip_ae.write(originsPath + 'test_ae.bpmn',AppEnsemble.bpmn_filename)
             zip_ae.write(originsPath + 'max_test.ttl','apps/max_test.ttl')
             zip_ae.write(originsPath + 'min_test.ttl','apps/min_test.ttl')
         finally:
@@ -62,22 +52,19 @@ class AppEnsembleTests(unittest.TestCase):
         self.assertTrue(self.ae.has_file('apps/max_test.ttl'))
         self.assertFalse(self.ae.has_file('apps/notexist.ttl'))
         self.assertNotEqual(self.ae.get_file('apps/max_test.ttl'),b'')
+        self.assertRaises(IOError,self.ae.get_file,'apps/notexist.ttl')
 
-    def test_remove_app_ensemble(self):
-        #
-        pass
-
-    def test_load_ae_model(self):
-        #
-        pass
-
-    def test_remove_app_ensemble(self):
-        #
-        #self.ae.remove_app_ensemble()
-        #os.remove(self.destTestArchive)
-        #zip_ae = zipfile.ZipFile(self.destTestArchive, mode='r')
-        pass
+    # TODO
+    #def test_load_ae_model(self):
+        # Don't know how to test!
+        #self.ae.load_ae_model(self.destTestArchive + '/test_ae_empty.ttl')
 
     def test_getRequiredApps(self):
-        #
-        pass
+        # Tests if the amount of required apps is correct
+        self.assertIs(len(self.ae.getRequiredApps().bindings),8,'Required Apps are not loaded properly')
+
+    def test_delte_AppEnsemble(self):
+        # Checks whether the AppEnsemble counter is decremented after deleting one
+        count=AppEnsemble.counter-1
+        self.ae.__del__()
+        self.assertIs(count,AppEnsemble.counter,'AppEnsemble was not deleted properly')
