c4bb44da6a7a7344027d8f82d5467402b9167e5a
khörfurter <korbinian.hoerfurter@tu-dresden.de>
2015-05-06 17:43:00 +0200

    Adapted the neccessary files to the outsorce of the AppPoll and AppEnsemble-Path

--- aof/tests/test_Views_integration.py
+++ aof/tests/test_Views_integration.py
@@ -234,6 +234,7 @@ class IntegrationViewTests(unittest.TestCase):
 
         response = views.AppEnsembleViews(self.context, self.request).ae_details_view()
         self.assertEqual(response['ae_uri'], 'testAppEnsemble')
+
         #self.assertTrue(len(response['ae_apps']) > 1)
 
         self._standard_tests(response)
