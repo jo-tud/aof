f427cc16926d0f47900e0c42f74d2405e264129e
khörfurter <korbinian.hoerfurter@tu-dresden.de>
2015-06-24 18:26:52 +0200

    Refactored routes for new API and URL-Structure

--- aof/__init__.py
+++ aof/__init__.py
@@ -21,19 +21,15 @@ def main(global_config, **settings):
     
     # Routes
     config.add_route('home', '/')
-    config.add_route('orchestrate', '/orchestrate.html')
 
     # App-Ensembles
     config.add_route('app-ensembles', '/app-ensembles.html')
-    config.add_route('ae-details', '/app-ensembles/details.html')
-    config.add_route('ae-details-new','/app-ensembles/{URI}/details')
-    config.add_route('ae-visualize-bpm', '/app-ensembles/visualize-bpm.html')
+    config.add_route('ae-details', '/app-ensembles/{URI}/details.html')
+    config.add_route('ae-visualize-bpm', '/app-ensembles/{URI}/bpm.html')
 
     # App-Pool
-    config.add_route('app-pool', '/apps.html')
-    config.add_route('app-details','/apps/details.html')
-    #config.add_route('app-details-new','/apps/{URI:.*}/{a:.*}') # URI-Übergabe funktioniert nicht!!!
-
+    config.add_route('apps', '/apps.html')
+    config.add_route('app-details','/apps/{URI:.*}/details.html')
 
     # Documentation
     config.add_route('documentation', '/docs/index.html')
@@ -41,37 +37,26 @@ def main(global_config, **settings):
     config.add_route('documentation-resource', '/resources/{document}')
 
     # API
-    ## JSON
-    config.add_route('api-ae-json', '/api/app-ensembles.json')
-    config.add_route('api-ap-json', '/api/app-pool.json')
-
-    ## BPM
-    config.add_route('ae-bpmn', '/app-ensembles/get-bpmn')
 
     ## Actions
     config.add_route('action-update-app-pool', '/api/actions/update-app-pool')
     config.add_route('action-update-ap-ensembles', '/api/actions/update-app-ensembles')
 
-    ## Downloads
-    config.add_route('api-get-ae-pkg', '/api/download/ae-package')
-
-    ## TTL
-    config.add_route('api-app-details','/api/app-pool/details.html')
-    config.add_route('api-app-details-show','/api/app-pool/details-show.html')
-
     # API NEW
-    config.add_route(pattern='/api/apps', name='api-apps') # returns app-pool in certain format
+    config.add_route(pattern='/api/apps', name='api-apps') # TODO  certain format
     config.add_route(pattern='/api/apps/{URI:.*}/details', name='api-apps-app-details')
     config.add_route(pattern='/api/apps/{URI:.*}/version', name='api-apps-app-version')
     # TODO
     #config.add_route(pattern='/api/apps/{URI:.*}/apk', name='api-apps-app-apk')
     # Todo complexe Property wird mit node ausgegeben
     config.add_route(pattern='/api/apps/{URI:.*}/properties/{property:.*}', name='api-apps-app-property')
-
+    # TODO: jquery put ermöglichen
     config.add_route(pattern='/api/apps', request_method='put', name='api-apps-update') #update
-    #config.add_route(pattern='/api/app-ensembles', name='api-appensembles')
+    config.add_route(pattern='/api/app-ensembles', name='api-appensembles')
     #config.add_route(pattern='/api/app-ensembles/{URI:.*}/details', name='api-appensembles-ae')
     #config.add_route(pattern='/api/app-ensembles/{URI:.*}/bpmn', name='api-appensembles-ae-bpmn')
+    config.add_route(pattern='/api/app-ensembles/{URI:.*}/package', name='api-appensembles-ae-package')
+    config.add_route(pattern='/api/app-ensembles/{URI:.*}/bpmn', name='api-appensembles-ae-bpmn')
 
 
 
@@ -80,20 +65,8 @@ def main(global_config, **settings):
     settings['mako.directories'] = os.path.join(here, 'templates')
 
 
-
-
     # Clear all temporary files
     aof.tmp.clear_all_tmp_files()
     # for a cronjob: function located in aof/tmp/__init__.py -> clear_all_tmp_files()
 
     return config.make_wsgi_app()
-
-
-
-"""
-    config.add_route(pattern='/api/apps/*/details/*', name='') # *= pool, http%3A%2F%2Fdev.plt.et.tu-dresden.de%3A8085%2Fjenkins%2Fjob%2FAppEnsembleInstaller%2FlastSuccessfulBuild%2F
-
-    config.add_route(pattern='/api/apps/apk', name='')
-    config.add_route(pattern='/api/apps/version', name='')
-    config.add_route(pattern='/api/apps/qrcode', name='')
-    config.add_route(pattern='/api/app-pool/qrcode', name='')"""
