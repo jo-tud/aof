cadfa4b448d666e61b823d25c34ad16321911c3f
khörfurter <korbinian.hoerfurter@tu-dresden.de>
2015-07-01 15:20:06 +0200

    Adapted AppEnsemblePool form file-based to ttl-based containment

--- aof/orchestration/AppEnsemblePool.py
+++ aof/orchestration/AppEnsemblePool.py
@@ -1,5 +1,5 @@
 __author__ = 'Korbinian Hörfurter'
-
+from rdflib import  URIRef,Literal
 from aof.orchestration.Singleton import Singleton
 from aof.orchestration.AppEnsemble import AppEnsemble
 from aof.orchestration.AOFGraph import AOFGraph
@@ -25,6 +25,7 @@ class AppEnsemblePool(AOFGraph):
     - returns a specific AppEnsemble-Object (get_AppEnsemble)
     - relaods the AppEnsemble-Pool with a new path (set_ae_folder_path)
     """
+    #TODO
     def __init__(self):
         """
         Set up the AppEnsemblePool and load the AppEnsembles from the standard path
@@ -45,6 +46,8 @@ class AppEnsemblePool(AOFGraph):
         self.a = AssetResolver()
         self._load_AppEnsembles()
 
+        self.itemtype=URIRef("http://comvantage.eu/ontologies/iaf/2013/0/Orchestration.owl#AppEnsemble")
+
 
 
     def __contains__(self, identifier):
@@ -53,16 +56,26 @@ class AppEnsemblePool(AOFGraph):
         :param identifier: String (Name of the AppEnsemble i.e. testAppEnsemble.ae-> item=testAppEnsemble
         :return:Boolean
         """
-        identifier=str(identifier)
-        return identifier in self.pool
+        q ="""
+            ASK
+            WHERE
+            {{?s  a <{0}>.?s  <{1}> "{2}".}}
+            """.format(self.itemtype,URIRef("http://comvantage.eu/ontologies/iaf/2013/0/Orchestration.owl#Name"),Literal(identifier))
+
+        return self.query(q).askAnswer
 
     def __len__(self):
         """
         Counts the number of AppEnsembles.
         :return:Integer
         """
-        return len(self.pool)
+        q = """
+        SELECT DISTINCT ?app
+        WHERE {{?app a <{0}> .}}
+        """.format(self.itemtype)
+        return len(self.query(q).bindings)
 
+    #TODO
     def __str__(self):
         """
         Returns string with information about the Location and Number of AppEnsembles.
@@ -71,6 +84,7 @@ class AppEnsemblePool(AOFGraph):
         ae_list=", ".join(self.pool)
         return '___AppPoolManager-Details:___\nAppEnsemble-Location: {}\nAppEnsemble-List ({}): {}'.format(self.get_ae_folder_path(),self.__len__(),ae_list)
 
+    #TODO
     def _load_AppEnsembles(self):
         """
         Indexes the AppEnsemble-Directory for files with the AppEnsemble-Extension and ADDs them to the AppEnsemblePool.
@@ -83,6 +97,8 @@ class AppEnsemblePool(AOFGraph):
                     identifier=file.replace(AppEnsemble.ae_extension,'')
                     ae_tmp=AppEnsemble(identifier)
                     self.pool[identifier]=ae_tmp
+            ###########################################################  Hier weitermache... prüfen ob das wirklich eingelesen wird... etc.
+            self.parse(source=os.path.join(self.get_ae_folder_path(),'testAppEnsemble','ae.ttl'), format="turtle")
             return None
         except FileNotFoundError as detail:
             if self._ae_folder_path != self._ae_folder_path_backup:
@@ -91,14 +107,14 @@ class AppEnsemblePool(AOFGraph):
             else:
                 self.log.error('AppEnsemble-Path "{}" was not found in the system!'.format(self.get_ae_folder_path()))
             return None
-
+    #TODO
     def get_ae_folder_path(self):
         """
         Returns the absolute AppEnsemble-Folder-path
         :return:String
         """
         return self.a.resolve(self._ae_folder_path).abspath()
-
+    #TODO
     def set_ae_folder_path(self,path):
         """
         Sets the new AppEnsemble-Folderpath and reloads the Apps into the AppEnsemblePool.
@@ -108,7 +124,7 @@ class AppEnsemblePool(AOFGraph):
         self._ae_folder_path=path
         self._load_AppEnsembles()
         return None
-
+    #TODO
     def reload(self):
         """
         CLEARs the AppEnsemble-Pool and reloads the AppEnsembles into the pool.
@@ -125,7 +141,7 @@ class AppEnsemblePool(AOFGraph):
         :return:Boolean
         """
         return self.__contains__(identifier)
-
+    #TODO
     def get_AppEnsemble(self,identifier):
         """
         Searches for a specific AppEnsemble
